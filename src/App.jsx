511ce341e51a22e0a5678fa07e902d5c39724345
CharanTeja <rcharanteja2006@gmail.com>
2025-07-29 12:37:24 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
       <h1>Welcome to My React Project - S205</h1>
       <p>CI / CD DevOps</p>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+      <p>Done with Git Pages and Git Actions</p>
     </div>
   );
 }
